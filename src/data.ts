17353c6fa6240238c26e52c84b455c5baa52c6ef
João Paulo <jpborella1@gmail.com>
2025-11-13 13:42:20 -0300

    substituir imagens JPG por PNG e atualizar referências no data.ts

--- src/data.ts
+++ src/data.ts
@@ -95,32 +95,32 @@ export const data = {
   produtos: [
     {
       id: 1,
-      imagem: "processos.jpg",
-      imagemResp: "processos-resp.jpg",
+      imagem: "processos.png",
+      imagemResp: "processos-resp.png",
       titulo: "Domínio Processos",
       descricao:
         "O Domínio Processos é um módulo adicional que pode ser adquirido pelos usuários do Domínio. Agora você conta com um módulo gerenciador de tarefas contábeis totalmente integrado ao Domínio que permite otimizar e gerenciar tarefas, ganhando mais tempo para ser estratégico. Automatize sua rotina com mais eficiência. Transforme a maneira como você trabalha com ainda mais tecnologia e agilidade. Adquira o Domínio Processos.",
     },
     {
       id: 2,
-      imagem: "messenger.jpg",
-      imagemResp: "messenger-resp.jpg",
+      imagem: "messenger.png",
+      imagemResp: "messenger-resp.png",
       titulo: "Domínio Messenger",
       descricao:
         "O Domínio Messenger é um software de comunicação para contadores que reinventa a relação com seus clientes, centralizando toda a comunicação de uma forma mais segura. Integrado ao Domínio, e usando o poder da Inteligência Artificial, fica mais fácil automatizar o atendimento do cliente do seu escritório, com as solicitações recebidas via atendimento por WhatsApp e a execução de mais de 25 tarefas sem qualquer intervenção manual.",
     },
     {
       id: 3,
-      imagem: "kolossus.jpg",
-      imagemResp: "kolossus-resp.jpg",
+      imagem: "kolossus.png",
+      imagemResp: "kolossus-resp.png",
       titulo: "Kolossus Auditor",
       descricao:
         "O Kolossus Auditor é o novo módulo de auditoria fiscal que pode ser adquirido pelos clientes que possuem o produto Domínio. Agora, você terá além de auditoria de obrigações fiscais e cruzamento de informações, novas funcionalidades para gerar cada vez mais rentabilidade e menos perdas. Isso significa que além de minimizar os riscos das informações de forma rápida e com embasamento técnico, você pode gerar novas soluções e agregar valor ao seu serviço, ganhando ainda mais a confiança e a satisfação dos seus clientes.",
     },
     {
       id: 4,
-      imagem: "web.jpg",
-      imagemResp: "web-resp.jpg",
+      imagem: "web.png",
+      imagemResp: "web-resp.png",
       titulo: "Domínio Web",
       descricao:
         "Escolha a liberdade como parceiro de negócios. Conecte-se ao Domínio de onde estiver e quando quiser com um sistema contábil na nuvem. Trabalhe em nuvem, sempre protegido e com segurança da informação. Com a Amazon, o Domínio Web garante a integridade de informações, protegendo o banco de dados do escritório contra vírus e hackers. Tenha mais praticidade para realizar consultorias externas, e torne mais eficientes as suas tarefas. Tudo com segurança, performance e estabilidade. Escolha ter mais para o seu negócio.",
